e18c240d16277e278a8cd5a99267e33bf6bbd93a
Sawyer Maloney <sawyerdmaloney@gmail.com>
2023-11-04 15:44:00 -0400

    adding changes

--- scratchFile.py
+++ scratchFile.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 import mongo # our file
 
 class NewUserLoginWindow:
+    widgets = []
     def __init__(self, root):
         self.root = root
         self.root.title("New User")
@@ -28,16 +29,23 @@ class NewUserLoginWindow:
         dropdown = tk.OptionMenu(root, teacher_subject, "Option 1", "Option 2", "Option 3", "Option 4")
         dropdown.pack()
 
+        self.widgets.append(self.username_label)
+        self.widgets.append(self.username_entry)
+        self.widgets.append(self.password_label)
+        self.widgets.append(self.password_entry)
+        self.widgets.append(dropdown)
+
 class ExistingUserLoginWindow:
+    widgets = []
     def __init__(self, root):
         self.root = root
-        self.root.title("Login Page")
 
         # Create labels and entry fields for username and password
         self.username_label = tk.Label(root, text="Username:")
         self.username_label.pack()
         self.username_entry = tk.Entry(root)
         self.username_entry.pack()
+
 
         self.password_label = tk.Label(root, text="Password:")
         self.password_label.pack()
@@ -47,6 +55,12 @@ class ExistingUserLoginWindow:
         # Create a login button
         self.login_button = tk.Button(root, text="Login", command=self.login)
         self.login_button.pack()
+
+        self.widgets.append(self.username_label)
+        self.widgets.append(self.username_entry)
+        self.widgets.append(self.password_label)
+        self.widgets.append(self.password_entry)
+        self.widgets.append(self.login_button)
 
     def login(self):
         # Get the entered username and password
@@ -98,8 +112,6 @@ class MainApplication:
 
     def DestroyAllWidgets(self, _widgets):
         for widget in _widgets:
-            text = widget.cget("text")
-            print(f"destroying widget: {text}")
             widget.destroy()
         widgets = []
 
