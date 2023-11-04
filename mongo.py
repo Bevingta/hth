b37e6a2be04bd20874d0ae01ebf762a8954c0187
Sawyer Maloney <sawyerdmaloney@gmail.com>
2023-11-04 16:30:40 -0400

    mongo changes

--- mongo.py
+++ mongo.py
@@ -30,12 +30,19 @@ def create(username, password): # add a username and associated password
 
 pdfs = []
 
-def insert_pdf(file_name, file_path):
+def insert_pdf(file_name, file_path, grade, title, subject):
+    pdfs = db["pdfs"]
     with open(file_path, 'rb') as f:
         a = fs.put(f)
-        pdfs.append(a)
-        #string = base64.b64encode(f.read())
-        #pdf.insert_one({"file_name": file_name, "pdf_data": string})
+        pdfs.insert_one({
+                "file_name": file_name,
+                "grade": grade,
+                "title": title,
+                "rating": 0,
+                "number_of_ratings": 0,
+                "ref": a,
+                "subject": subject
+            })
     
 
 def read_and_save_pdf(file_name):
@@ -52,3 +59,28 @@ def validate_user(username, password):
     else:
         print("User found")
         return True
+
+def search_for_pdf(subject, grade):
+    pdfs = db["pdfs"]
+    results = pdfs.find({"subject": subject, "grade": grade})
+
+    for result in results:
+        file_name = result["file_name"]
+        grade = result["grade"]
+        title = result["title"]
+        rating = result["rating"]
+        print(f"fileName: {file_name}, grade: {grade}, title: {title}, rating: {rating}")
+
+
+insert_pdf("a", "a.pdf", 12, "The Science of everything", "Science")
+insert_pdf("b", "b.pdf", 11, "The Math of everything", "Math")
+insert_pdf("c", "c.pdf", 9, "The hist", "English")
+insert_pdf("d", "d.pdf", 8, "d.pdf", "History")
+
+search_for_pdf("Science", 12)
+
+
+
+
+
+
