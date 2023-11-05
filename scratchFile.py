fa222c91c8a6a1b3d2f57d7a1cb5c238ff58667b
Sawyer Maloney <sawyerdmaloney@gmail.com>
2023-11-05 11:25:18 -0500

    return to login page

--- scratchFile.py
+++ scratchFile.py
@@ -265,11 +265,26 @@ class NewUserLoginWindow:
         self.create_button = tk.Button(root, text="Create User", command=self.user_pass_to_main)
         self.create_button.pack()
 
+        returnButton = tk.Button(root, text="Return to Login Screen", command=self.return_to_login)
+        returnButton.pack(side=tk.BOTTOM)
+        
+        self.widgets.append(returnButton)
         self.widgets.append(self.username_label)
         self.widgets.append(self.username_entry)
         self.widgets.append(self.password_label)
         self.widgets.append(self.password_entry)
         self.widgets.append(drop)
+
+    def return_to_login(self):
+        for widget in self.widgets:
+            try:
+                widget.destroy()
+            except:
+                print("failed to destroy a widget")
+        self.widget = []
+        self.root.destroy()
+        root = tk.Tk()
+        MainApplication(root)
 
 
 class ExistingUserLoginWindow:
@@ -345,21 +360,24 @@ class MainApplication:
     def open_existing_user_login_page(self):
         self.root.title("Existing User Login")
 
-        self.DestroyAllWidgets(self.widgets)
+        self.DestroyAllWidgets()
 
         login_window = ExistingUserLoginWindow(self.root)
 
     def open_new_user_login_page(self):
         self.root.title("New User")
 
-        self.DestroyAllWidgets(self.widgets)
+        self.DestroyAllWidgets()
 
         login_window = NewUserLoginWindow(self.root)
 
-    def DestroyAllWidgets(self, _widgets):
-        for widget in _widgets:
-            widget.destroy()
-        widgets = []
+    def DestroyAllWidgets(self):
+        for widget in self.widgets:
+            try:
+                widget.destroy()
+            except:
+                print("failed to destroy a widget")
+        self.widgets = []
 
 if __name__ == "__main__":
     root = tk.Tk()
